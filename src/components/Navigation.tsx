52071ff5147ac1c8e0b4be7e91a8348dd6c2d3f3
sirajahsraf <sirajashraf707@gmail.com>
2025-09-17 11:40:19 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -57,7 +57,7 @@ export function Navigation({
         <div className="flex items-center gap-4">
           <h1 className="text-base sm:text-lg font-bold text-white font-['Space_Grotesk']">
             <span className="hidden sm:inline">
-              Prabin Thakur{" "}
+              Siraj Ashraf {" "}
             </span>
             <span className="sm:hidden">Portfolio</span>
           </h1>
